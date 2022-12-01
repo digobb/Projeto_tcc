263c2884fd35da8b28799a783814b6ca7dfc8784
digobb <165532@upf.br>
2022-11-30 22:34:23 -0300

    ajustado menu, adicionado verificacao de data

--- menu_jogos.py
+++ menu_jogos.py
@@ -1,20 +1,10 @@
-from distutils.cmd import Command
-from sqlite3 import Row
 from tkinter import *
-from tkinter import ttk
-from turtle import position
-from PIL import Image
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 from dashboard_estatisticas import grafico_partida as gp
 from api import *
 from manipulando_retorno_api import chama_retorno_jogos_aovivo
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import os, time
-
-#dados = pd.read_csv(r'C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/teste_dtf.csv')
+from datetime import date
 
 def dados_partida_id(id):
     print(id)
@@ -30,25 +20,41 @@ def tela_principal():
     tela['background']='white'
     tela.iconbitmap('C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/img/icon.ico')
     tela.title('Jogos ao vivo')
-    tela.geometry("400x400") 
-    tela.minsize(400, 400) 
+    tela.geometry("400x800") 
+    tela.minsize(400, 400)
+    tela.resizable(False, False)
 
-    if os.path.exists(f'C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv'):
-        print('Arquivo com dados da partida encontrados!')
+    img_logo = PhotoImage(file='C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/img/campeonato_brasileiro.png', width=150, height=150)
+    logo     = Label(tela, image=img_logo)
+    logo.img_logo = img_logo
+    logo.place(x=120, y=3)
+
+    data_atual = date.today()
+    data_atual = data_atual.strftime('%d/%m/%Y')
+
+    if os.path.exists('C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv'):
+        print('Arquivo contendo os jogos ao vivo encontrado, verificando...')
         dados = pd.read_csv(f'C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv')
+        if dados.loc[0, 'data_realizacao'] != data_atual:
+            print('Atualizando partidas...')
+            os.remove('C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv'), time.sleep(1)
+            chama_retorno_jogos_aovivo(), time.sleep(1.5)
+            dados = pd.read_csv(f'C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv')
+        else:
+            dados = pd.read_csv(f'C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv')
     else:
-        print('Nao foi encontrado arquivo com os jogos ao vivo... criando...')
-        chama_retorno_jogos_aovivo()
-        time.sleep(2)
+        print('Não foi encontrado arquivo com os jogos ao vivo... criando...')
+        chama_retorno_jogos_aovivo(), time.sleep(1.5)
         dados = pd.read_csv(f'C:/Users/didico/Documents/Projeto_TCC/projeto_tcc/arquivo/jogos_aovivo.csv')
     
-    pos_y = int(0)
+    pos_y = int(135)
     for index, column in dados.iterrows():
         id_partida = column['_link']
         dados.loc[0, '_link'] = Button(tela, text=f'{column["time_mandante"]} x {column["time_visitante"]}', font=('', 12), bg='#305EE4', fg='white')
         dados.loc[0, '_link']['command'] = lambda id=id_partida: dados_partida_id(id)
         pos_y = pos_y + 35
-        dados.loc[0, '_link'].place(x=100, y=pos_y)
+        dados.loc[0, '_link'].place(x=110, y=pos_y)
+
     tela.mainloop()
 
 tela_principal()
